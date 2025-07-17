9a16e1c1da63031548ca56b97e3ea786aead0efe
pirk8679 <42756220+pirk8679@users.noreply.github.com>
2025-07-17 13:41:48 +0900

    Add files via upload
    test 최초 커밋

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello vue
+  </div>
+</template>
+
+<script setup>
+// 아무 코드도 필요 없음!
+</script>
+
+<style>
+/* 원하면 간단한 스타일 추가 */
+body, html, #app { height: 100%; }
+div { font-size: 2rem; text-align: center; margin-top: 40vh; }
+</style>
